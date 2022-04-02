64fb6cdb6e0610fd77113214618db75de3385e78
Shane Wilson <shane.wilson@Shane-Wilson-MBP16.local>
2022-04-01 22:31:35 -0500

    chapter 45 complete

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,13 @@
+const path = require('path')
 const express = require('express')
 
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-  res.send('<h1>Hello express!</h1>')
-})
-
-app.get('/help', (req, res) => {
-  res.send({
-    name: 'Shane',
-    age: 51
-  })
-})
-
-app.get('/about', (req, res) => {
-  res.send('<h1>ABOUT page.</h1>')
-})
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
   res.send({
