810302d0cfe0ef2fd8591d7dec604653094a1472
Shane Wilson <shane.wilson@Shane-Wilson-MBP16.local>
2022-04-05 11:29:38 -0500

    chapter 47

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,33 @@
 const path = require('path')
 const express = require('express')
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
-
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Shane D Wilson'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Shane D Wilson'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Page',
+    name: 'Shane D Wilson',
+    message: 'This is a sample help message.'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
