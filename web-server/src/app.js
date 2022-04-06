597016abfde3d6b9820ced6b1911279d7486aa68
Shane Wilson <40958758+shanedwilson@users.noreply.github.com>
2022-04-06 10:05:24 -0500

    Section7 (#1)
    * chapter 43 complete
    
    * chapter 44 complete
    
    * chapter 45 complete
    
    * added gitignore
    
    * adding gitignore
    
    * chapter 46 complete
    
    * chapter 47
    
    * chapter 49 complete
    
    Co-authored-by: Shane Wilson <shane.wilson@Shane-Wilson-MBP16.local>

---
+++ web-server/src/app.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Shane D Wilson'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Shane D Wilson'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Page',
+    name: 'Shane D Wilson',
+    message: 'This is a sample help message.'
+  })
+})
+
+app.get('/weather', (req, res) => {
+  res.send({
+    forecast: 'Stormy',
+    location: 'My butt'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.')
+})
