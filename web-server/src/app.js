4e6fca69f8727382894c1139ed9530f5bda198e5
Shane Wilson <40958758+shanedwilson@users.noreply.github.com>
2022-04-06 11:17:44 -0500

    Section7 (#2)
    * chapter 43 complete
    
    * chapter 44 complete
    
    * chapter 45 complete
    
    * added gitignore
    
    * adding gitignore
    
    * chapter 46 complete
    
    * chapter 47
    
    * chapter 49 complete
    
    * chapter 50 complete
    
    * section7 complete
    
    Co-authored-by: Shane Wilson <shane.wilson@Shane-Wilson-MBP16.local>

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -46,6 +46,22 @@ app.get('/weather', (req, res) => {
   })
 })
 
+app.get('/help/*', (req, res) =>{
+  res.render('404', {
+    title: '404',
+    name: 'Shane D Wilson',
+    message: 'Help article not found.'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Shane D Wilson',
+    message: 'Page not found.'
+  })
+})
+
 app.listen(3000, () => {
   console.log('Server is up on port 3000.')
 })
