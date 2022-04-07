f96b9d96c43e0eb83c739d6c1589b1af5ce76ea5
Shane Wilson <shane.wilson@Shane-Wilson-MBP16.local>
2022-04-07 18:01:56 -0500

    chapter 56 complete

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -40,9 +42,42 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+  const address = req.query.address
+  if (!address) {
+    return res.send({
+      error: 'You must provide an address'
+    })
+  }
+
+  geocode(address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error })
+    }
+  
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error })
+      }
+  
+      res.send({
+        forecast: forecastData,
+        location,
+        address,
+      })
+    })
+  }) 
+})
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
+
+  console.log(req.query)
   res.send({
-    forecast: 'Stormy',
-    location: 'My butt'
+    products: [],
   })
 })
 
